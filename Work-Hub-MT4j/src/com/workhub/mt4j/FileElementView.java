20483318335f625d339a4461f6310ad26c4de9a0
Maerig <jean-Loup.roussel-clouet@etu.utc.fr>
2013-05-29 17:57:08 +0200

    Permet de modifier le contenu d'un post-it fichier

--- Work-Hub-MT4j/src/com/workhub/mt4j/FileElementView.java
+++ Work-Hub-MT4j/src/com/workhub/mt4j/FileElementView.java
@@ -1,6 +1,7 @@
 package com.workhub.mt4j;
 
 import org.mt4j.components.TransformSpace;
+import org.mt4j.components.visibleComponents.shapes.MTRectangle.PositionAnchor;
 import org.mt4j.components.visibleComponents.widgets.MTImage;
 import org.mt4j.util.math.Vector3D;
 
@@ -44,7 +45,20 @@ public class FileElementView extends AbstractElementView {
 	}
 
 	public void editElementContent() {
-		// TODO
+		String filePath = mtApplication.selectInput();
+		updateTitleWithElementPath(filePath);
+		PImage image = mtApplication.loadImage(chooseIcon());
+		if ( image.height < image.width )
+			image.resize((int) (this.getWidthXYGlobal()-10), 0);
+		else image.resize(0, (int) (this.getHeightXYGlobal()-40));
+		content.removeFromParent();
+		content = new MTImage(image, mtApplication);
+		content.setNoFill(true);
+		content.setPickable(false);
+		content.setNoStroke(true);
+		content.setAnchor(PositionAnchor.UPPER_LEFT);
+		content.setPositionGlobal(new Vector3D(this.getPosition(TransformSpace.LOCAL).getX(), (float) (this.getPosition(TransformSpace.LOCAL).getY()+40)));
+		addChild(content);
 	}
 
 	public MTImage getContent() {
