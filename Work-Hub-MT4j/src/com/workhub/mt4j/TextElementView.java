f92293bcc1b49c1419fed978f5ee302581e5fd4e
Maerig <jean-Loup.roussel-clouet@etu.utc.fr>
2013-06-05 11:44:39 +0200

    Retour automatique à la ligne pour les textes

--- Work-Hub-MT4j/src/com/workhub/mt4j/TextElementView.java
+++ Work-Hub-MT4j/src/com/workhub/mt4j/TextElementView.java
@@ -1,10 +1,7 @@
 package com.workhub.mt4j;
-import org.mt4j.components.TransformSpace;
 import org.mt4j.components.visibleComponents.font.FontManager;
 import org.mt4j.components.visibleComponents.widgets.MTTextArea;
 import org.mt4j.util.MTColor;
-import org.mt4j.util.math.Vector3D;
-
 import processing.core.PApplet;
 
 public class TextElementView extends AbstractElementView {
@@ -14,15 +11,17 @@ public class TextElementView extends AbstractElementView {
 	public TextElementView(float x, float y, float width,
 			float height, PApplet applet, WorkHubScene scene) {
 		super(x, y, Constants.Z_POSITION_DEFAULT_ELEMENT, width, height, applet, scene);
-		content = new MTTextArea(applet, FontManager.getInstance().createFont(
-				applet, "arial.ttf", 18, new MTColor(50, 50, 50, 255),
-				new MTColor(0, 0, 0, 255)));
+		content = new MTTextArea(x,
+				y + 40,
+				width,
+				height - 40,
+				FontManager.getInstance().createFont(applet, "arial.ttf", 18, new MTColor(50, 50, 50, 255), new MTColor(0, 0, 0, 255)),
+				applet
+				);
 		content.setNoFill(true);
 		content.setText("Ajoutez votre texte ici");
 		content.setPickable(false);
 		content.setNoStroke(true);
-		content.setAnchor(PositionAnchor.UPPER_LEFT);
-		content.setPositionGlobal(new Vector3D(this.getPosition(TransformSpace.GLOBAL).getX(), (float) (this.getPosition(TransformSpace.GLOBAL).getY()+40)));
 		addChild(content);
 	}
 
