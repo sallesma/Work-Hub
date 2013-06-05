d6a05b77a32d5b9f110897140cc8e9f9bc28edee
louise <louve.grandjonc@gmail.com>
2013-06-05 03:00:20 +0200

    clean code

--- Work-Hub-MainLib/src/com/workhub/jade/behaviour/EditableElementBehaviour.java
+++ Work-Hub-MainLib/src/com/workhub/jade/behaviour/EditableElementBehaviour.java
@@ -50,20 +50,7 @@ public class EditableElementBehaviour extends CyclicBehaviour{
 				AID receiver = message.getSender();
 				
 				ACLMessage answer = MessageFactory.createMessage((ElementAgent)myAgent, receiver, action);
-				/*ACLMessage answer = new ACLMessage(ACLMessage.INFORM);
-				JsonObject j = new JsonObject();
-
-				System.out.println("editor : "+((ElementAgent)myAgent).getEditor());
-				boolean autorization = is_editable();
-				System.out.println("editor : "+((ElementAgent)myAgent).getEditor());
-				j.addProperty("can_edit", autorization);
-				j.addProperty(Constants.JSON_ACTION, Constants.MESSAGE_ACTION_EDIT);
-				String content = j.toString();
-				answer.setContent(content);
-				answer.setSender(myAgent.getAID());
-				answer.addReceiver(message.getSender());
-				System.out.println("je vais envoyer : "+answer);*/
-
+				
 				myAgent.send(answer);				
 				break;
 			}
@@ -78,11 +65,9 @@ public class EditableElementBehaviour extends CyclicBehaviour{
 	public boolean is_editable(){
 		
 		if(((ElementAgent)myAgent).getEditor() != null){
-			System.out.println("je sais qu'il y a un editeur : "+((ElementAgent)myAgent).getEditor());
 			AID editor = ((ElementAgent)myAgent).getEditor();
 			DFAgentDescription[] result = Utils.agentSearch(myAgent, Constants.CLIENT_AGENT);
 			boolean ok = false;
-			System.out.println("liste agent client"+result);
 			
 			for(DFAgentDescription df : result){
 				if(df.getName() == editor){
@@ -91,12 +76,10 @@ public class EditableElementBehaviour extends CyclicBehaviour{
 				
 			}
 			if(!ok){
-				System.out.println("je n'ai pas trouve l'editure : "+((ElementAgent)myAgent).getEditor());
 				((ElementAgent)myAgent).setEditor(myAgent.getAID());
 				return true;
 			}else{
 				//TODO
-				System.out.println("what the fuck");
 				//envoyer une requete a l'agent éditeur pour confirmer qu'il édite tjrs
 				return false;
 			}
