b9b4c2633e39ea94be8ffd9a91629598d4632811
Maerig <jean-Loup.roussel-clouet@etu.utc.fr>
2013-06-06 15:42:18 +0200

    Migration des API Jade vers le singleton JadeInterface

---
+++ Work-Hub-MT4j/src/com/workhub/mt4j/JadeInterface.java
@@ -0,0 +1,199 @@
+package com.workhub.mt4j;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.workhub.jade.agent.ClientAgentInterface;
+import com.workhub.model.ElementModel;
+import com.workhub.utils.Constants;
+
+import jade.core.AID;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.gui.GuiEvent;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
+public final class JadeInterface implements PropertyChangeListener {
+	private static volatile JadeInterface instance;
+
+	private AgentController agentController;
+	private WorkHubScene scene;
+	
+	private JadeInterface() {
+	}
+	
+	public static JadeInterface getInstance() {
+		return instance;
+	}
+	
+	public void setScene(WorkHubScene scene) {
+		this.scene = scene;
+	}
+	
+	public void startJade(String hostID, String platformID, boolean isHost, String nickname) {
+		Runtime rt = Runtime.instance();
+		Profile profile = isHost ? new ProfileImpl(null , 1099, null) : new ProfileImpl(hostID, 1099, platformID, false);
+		ContainerController cc = rt.createMainContainer(profile);
+		try {
+			agentController = cc.createNewAgent(nickname, "com.workhub.jade.agent.ClientAgent", new Object[] { this });
+			agentController.start();
+		} catch (StaleProxyException e) {
+			System.err.println("Erreur lors de la creation de l'agent");
+			e.printStackTrace();
+		}
+	}
+
+	private ClientAgentInterface getAgent() throws StaleProxyException {
+		return agentController.getO2AInterface(ClientAgentInterface.class);
+	}
+	
+	private void fireOnGuiEvent(GuiEvent event) {
+		try {
+			getAgent().fireOnGuiEvent(event);
+		} catch (StaleProxyException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void sendElement(AID dest, AID elementAgent){
+		Object[] param = {dest , elementAgent};
+
+		GuiEvent event = new GuiEvent(param,Constants.EVENT_TYPE_SEND);
+		fireOnGuiEvent(event);
+	}
+	public void askEdition(AID elementAgent){
+		GuiEvent event = new GuiEvent(elementAgent,Constants.EVENT_TYPE_ASK_EDIT);
+		fireOnGuiEvent(event);
+	}
+
+
+	public void createElement(int elementType){
+		GuiEvent event = new GuiEvent(elementType,Constants.EVENT_TYPE_CREATE_ELEMENT);
+		fireOnGuiEvent(event);
+	}
+
+	public void deleteElement(AID elementAgent){
+		GuiEvent event = new GuiEvent(elementAgent,Constants.EVENT_TYPE_DELETE);
+		fireOnGuiEvent(event);
+	}
+
+	public void saveElement(ElementModel model){
+		GuiEvent event = new GuiEvent(model, Constants.EVENT_TYPE_SAVE);
+		fireOnGuiEvent(event);
+	}
+
+	public void getElement(AID agentAID ){
+		GuiEvent event = new GuiEvent(agentAID, Constants.EVENT_TYPE_CHARGE);
+		fireOnGuiEvent(event);
+	}
+
+	public void getElementList(){
+		GuiEvent event = new GuiEvent(null, Constants.EVENT_TYPE_GET_ELEMENTS);
+		fireOnGuiEvent(event);
+	}
+
+	public void getNeightbourgList(){
+		GuiEvent event = new GuiEvent(null, Constants.EVENT_TYPE_GET_NEIGHBOURGS);
+		fireOnGuiEvent(event);
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		switch ((Integer.parseInt(event.getPropertyName()))) {
+		case Constants.EVENT_TYPE_CHANGE:
+		{
+			AID aidModel = (AID)event.getNewValue();
+			AbstractElementView element = null;
+			try {
+				element = scene.getElement(aidModel);
+			} catch (WorkHubException e) {
+				e.printStackTrace();
+			}
+			if(element!=null){
+				getElement(aidModel);
+			}
+			break;
+		}
+		case Constants.EVENT_TYPE_CONTENU:
+		{
+			ElementModel model = (ElementModel)event.getNewValue();
+			AbstractElementView element = null;
+			try {
+				element = scene.getElement(model.getAgent());
+			} catch (WorkHubException e1) {
+				e1.printStackTrace();
+			}
+			if(element!=null){
+				element.setModel(model);
+			}else{
+				try {
+					scene.addElement(model);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			break;
+		}
+		case Constants.EVENT_TYPE_DIED:
+		{
+			ElementModel model = (ElementModel)event.getNewValue();
+			AbstractElementView element = null;
+			try {
+				element = scene.getElement(model.getAgent());
+			} catch (WorkHubException e) {
+				e.printStackTrace();
+			}
+			if(element!=null){
+				element.destroy();
+			}
+			break;
+		}
+		case Constants.EVENT_TYPE_ELEMENTS:
+		{
+			Map<AID, String> map = (Map<AID, String>)event.getNewValue();
+			
+			for (Entry<AID, String> entry : map.entrySet()) {
+				//scene.addToAdapter(entry); TODO
+			}
+			
+
+			break;
+		}
+		case Constants.EVENT_TYPE_NEIGHBOURS:
+		{
+			Map<AID, String> map = (Map<AID, String>)event.getNewValue();
+			for (Entry<AID, String> entry : map.entrySet()) {
+				//scene.addToAdapter(entry); TODO
+			}
+
+			break;
+		}
+		case Constants.EVENT_TYPE_CAN_EDIT:
+		{
+			AID aidModel = (AID)event.getNewValue();
+			AbstractElementView element = null;
+			try {
+				element = scene.getElement(aidModel);
+			} catch (WorkHubException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(element!=null){
+				element.edit();
+			}
+			break;
+		}
+		case Constants.EVENT_TYPE_CANT_EDIT:
+		{
+			// TODO visualisation de l'accès refusé		
+			break;
+		}
+		}
+
+	}
+}
--- Work-Hub-MT4j/src/com/workhub/mt4j/WorkHubLauncher.java
+++ Work-Hub-MT4j/src/com/workhub/mt4j/WorkHubLauncher.java
@@ -1,35 +1,11 @@
 package com.workhub.mt4j;
 
-import jade.core.AID;
-import jade.core.MicroRuntime;
-import jade.core.Profile;
-import jade.core.ProfileImpl;
-import jade.core.Runtime;
-import jade.gui.GuiEvent;
-import jade.util.leap.Properties;
-import jade.wrapper.AgentController;
-import jade.wrapper.ContainerController;
-import jade.wrapper.ControllerException;
-import jade.wrapper.StaleProxyException;
-
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import javax.swing.ImageIcon;
 
 import org.mt4j.MTApplication;
 
-import com.workhub.jade.agent.ClientAgent;
-import com.workhub.jade.agent.ClientAgentInterface;
-import com.workhub.model.ElementModel;
-import com.workhub.utils.Constants;
-
-public class WorkHubLauncher extends MTApplication implements PropertyChangeListener {
+public class WorkHubLauncher extends MTApplication {
 	private static final long serialVersionUID = -3337824151386218881L;
-	private WorkHubScene scene;
-	private AgentController agentController;
 
 	public static void main(String[] args) {
 		initialize();
@@ -38,171 +14,13 @@ public class WorkHubLauncher extends MTApplication implements PropertyChangeList
 	@Override
 	public void startUp() {
 		try {
-			addScene(new WorkHubScene(this, "WorkHub"));
+			WorkHubScene scene = new WorkHubScene(this, "WorkHub");
+			addScene(scene);
+			JadeInterface.getInstance().setScene(scene);
 			ImageIcon icon = new ImageIcon("Image/logo.png");
 			frame.setIconImage(icon.getImage());
 		} catch (WorkHubException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void startJade(String hostID, String platformID, boolean isHost, String nickname) {
-		Runtime rt = Runtime.instance();
-		Profile profile = isHost ? new ProfileImpl(null , 1099, null) : new ProfileImpl(hostID, 1099, platformID, false);
-		ContainerController cc = rt.createMainContainer(profile);
-		try {
-			agentController = cc.createNewAgent(nickname, "com.workhub.jade.agent.ClientAgent", new Object[] { this });
-			agentController.start();
-		} catch (StaleProxyException e) {
-			System.err.println("Erreur lors de la creation de l'agent");
-			e.printStackTrace();
-		}
-	}
-
-	private ClientAgentInterface getAgent() throws StaleProxyException {
-		return agentController.getO2AInterface(ClientAgentInterface.class);
-	}
-	
-	private void fireOnGuiEvent(GuiEvent event) {
-		try {
-			getAgent().fireOnGuiEvent(event);
-		} catch (StaleProxyException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void sendElement(AID dest, AID elementAgent){
-		Object[] param = {dest , elementAgent};
-
-		GuiEvent event = new GuiEvent(param,Constants.EVENT_TYPE_SEND);
-		fireOnGuiEvent(event);
-	}
-	public void askEdition(AID elementAgent){
-		GuiEvent event = new GuiEvent(elementAgent,Constants.EVENT_TYPE_ASK_EDIT);
-		fireOnGuiEvent(event);
-	}
-
-
-	public void createElement(int elementType){
-		GuiEvent event = new GuiEvent(elementType,Constants.EVENT_TYPE_CREATE_ELEMENT);
-		fireOnGuiEvent(event);
-	}
-
-	public void deleteElement(AID elementAgent){
-		GuiEvent event = new GuiEvent(elementAgent,Constants.EVENT_TYPE_DELETE);
-		fireOnGuiEvent(event);
-	}
-
-	public void saveElement(ElementModel model){
-		GuiEvent event = new GuiEvent(model, Constants.EVENT_TYPE_SAVE);
-		fireOnGuiEvent(event);
-	}
-
-	public void getElement(AID agentAID ){
-		GuiEvent event = new GuiEvent(agentAID, Constants.EVENT_TYPE_CHARGE);
-		fireOnGuiEvent(event);
-	}
-
-	public void getElementList(){
-		GuiEvent event = new GuiEvent(null, Constants.EVENT_TYPE_GET_ELEMENTS);
-		fireOnGuiEvent(event);
-	}
-
-	public void getNeightbourgList(){
-		GuiEvent event = new GuiEvent(null, Constants.EVENT_TYPE_GET_NEIGHBOURGS);
-		fireOnGuiEvent(event);
-	}
-
-	@Override
-	public void propertyChange(PropertyChangeEvent event) {
-		switch ((Integer.parseInt(event.getPropertyName()))) {
-		case Constants.EVENT_TYPE_CHANGE:
-		{
-			AID aidModel = (AID)event.getNewValue();
-			AbstractElementView element = null;
-			try {
-				element = scene.getElement(aidModel);
-			} catch (WorkHubException e) {
-				e.printStackTrace();
-			}
-			if(element!=null){
-				getElement(aidModel);
-			}
-			break;
-		}
-		case Constants.EVENT_TYPE_CONTENU:
-		{
-			ElementModel model = (ElementModel)event.getNewValue();
-			AbstractElementView element = null;
-			try {
-				element = scene.getElement(model.getAgent());
-			} catch (WorkHubException e1) {
-				e1.printStackTrace();
-			}
-			if(element!=null){
-				element.setModel(model);
-			}else{
-				try {
-					scene.addElement(model);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			break;
-		}
-		case Constants.EVENT_TYPE_DIED:
-		{
-			ElementModel model = (ElementModel)event.getNewValue();
-			AbstractElementView element = null;
-			try {
-				element = scene.getElement(model.getAgent());
-			} catch (WorkHubException e) {
-				e.printStackTrace();
-			}
-			if(element!=null){
-				element.destroy();
-			}
-			break;
-		}
-		case Constants.EVENT_TYPE_ELEMENTS:
-		{
-			Map<AID, String> map = (Map<AID, String>)event.getNewValue();
-			
-			for (Entry<AID, String> entry : map.entrySet()) {
-				//scene.addToAdapter(entry); TODO
-			}
-			
-
-			break;
-		}
-		case Constants.EVENT_TYPE_NEIGHBOURS:
-		{
-			Map<AID, String> map = (Map<AID, String>)event.getNewValue();
-			for (Entry<AID, String> entry : map.entrySet()) {
-				//scene.addToAdapter(entry); TODO
-			}
-
-			break;
-		}
-		case Constants.EVENT_TYPE_CAN_EDIT:
-		{
-			// TODO
-			/*
-			AID aidModel = (AID)event.getNewValue();
-			AbstractElementView element = scene.getElement(aidModel);
-			if(element!=null){
-				element.edit();
-			}
-			*/
-			break;
-		}
-		case Constants.EVENT_TYPE_CANT_EDIT:
-		{
-			// TODO visualisation de l'accès refusé		
-			break;
-		}
-		}
-
-	}
-
 }
