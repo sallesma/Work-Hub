c7d8ae66c7beeba0e12d4fb17e5ff2fc4a69f4c4
louise <louve.grandjonc@gmail.com>
2013-06-06 05:44:04 +0200

    share behaviour finished (problem with merge)

--- Work-Hub-MainLib/src/com/workhub/jade/behaviour/ShareClientBehaviour.java
+++ Work-Hub-MainLib/src/com/workhub/jade/behaviour/ShareClientBehaviour.java
@@ -1,23 +1,22 @@
 package com.workhub.jade.behaviour;
 
-import com.google.gson.JsonObject;
-
-import com.google.gson.JsonParser;
-import com.workhub.utils.Constants;
-
 import jade.core.AID;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.lang.acl.MessageTemplate.MatchExpression;
-import jade.util.leap.Iterator;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.workhub.jade.agent.ClientAgent;
+import com.workhub.utils.Constants;
+import com.workhub.utils.Utils;
 
 //Behaviour agent client
 public class ShareClientBehaviour extends CyclicBehaviour {
-	
-	
+
+
 	private MessageTemplate template = new MessageTemplate(new MatchExpression() {
 		@Override
 		public boolean match(ACLMessage msg) {
@@ -33,19 +32,40 @@ public class ShareClientBehaviour extends CyclicBehaviour {
 	});
 	@Override
 	public void action() {
-		
+
 		ACLMessage message = myAgent.receive(template);
 		if (message!=null){
-			
+
 			ClientAgent cli = (ClientAgent)myAgent;
 			// Un clientAgent recoit d'un autre clientAgent un message lui proposant de partager un Element
+
 			// on ajoute a la boite au lettre du clientAgent le message car l'element sera recupere au clic sur le bouton "recevoir"
-			
+
+
+			// on recupere dans le json du message l'AID de l'element a partager
+			JsonParser js = new JsonParser();
+			String agent = ((JsonObject) js.parse(message.getContent())).get("element").getAsString();
+
+			AID element_shared = findElementAgent(agent);
+
+			((ClientAgent)myAgent).fireChanges(Constants.EVENT_TYPE_RECEIVE_ELEMENT, element_shared);
 		}
 
-		
+
 		// Lorsque le clientAgent clic sur recevoir il recupere l'AID de l'ElementAgent partage et envoie un message de GET_CONTENT  a l'agent pour mettre a jour son interface avec le nouvel element
-		
+
+	}
+
+	private AID findElementAgent(String agent){
+		DFAgentDescription[] result = Utils.agentSearch(myAgent, Constants.ELEMENT_AGENT);
+
+		for(DFAgentDescription df : result){
+			if(df.getName().getName().equals(agent)){
+				return df.getName();
+			}
+		}
+		return null;
+
 	}
 
 }
