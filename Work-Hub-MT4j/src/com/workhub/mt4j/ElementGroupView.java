a08d25433d58e906ceb7c1fdf81e70dbff770a72
Maerig <jean-Loup.roussel-clouet@etu.utc.fr>
2013-06-09 10:26:30 +0200

    Raccourci masquer pour les groupes

--- Work-Hub-MT4j/src/com/workhub/mt4j/ElementGroupView.java
+++ Work-Hub-MT4j/src/com/workhub/mt4j/ElementGroupView.java
@@ -2,6 +2,10 @@ package com.workhub.mt4j;
 
 import org.mt4j.components.clusters.Cluster;
 import org.mt4j.components.visibleComponents.shapes.MTPolygon;
+import org.mt4j.input.inputProcessors.IGestureEventListener;
+import org.mt4j.input.inputProcessors.MTGestureEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragProcessor;
 import org.mt4j.util.math.Vector3D;
 
 import processing.core.PApplet;
@@ -9,9 +13,24 @@ import processing.core.PApplet;
 public class ElementGroupView extends Cluster {
 	private WorkHubScene scene;
 
-	public ElementGroupView(PApplet applet, WorkHubScene scene, MTPolygon selectionPolygon) {
+	public ElementGroupView(PApplet applet, final WorkHubScene scene, MTPolygon selectionPolygon) {
 		super(applet, selectionPolygon);
 		this.scene = scene;
+
+		addGestureListener(DragProcessor.class, new IGestureEventListener() {
+
+			@Override
+			public boolean processGestureEvent(MTGestureEvent ge) {
+				DragEvent de = (DragEvent)ge;
+				if(de.getId() == DragEvent.GESTURE_ENDED) {
+					String intersection = scene.testShortCutIntersection(de.getTo());
+					if(intersection.equals(MT4JConstants.BUTTON_ID_MASQUER)) {
+							ElementGroupView.this.destroy();
+					}
+				}
+				return false;
+			}
+		});
 	}
 	public void openContextualMenu(Vector3D locationOnScreen) {
 		ContextMenu contextMenu = new ContextMenu(this, (int)locationOnScreen.x, (int)locationOnScreen.y, getRenderer(), scene, MT4JConstants.CONTEXT_GROUP_MENU);
